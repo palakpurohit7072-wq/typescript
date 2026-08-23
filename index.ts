162f4d3201af474a2473aecc0b6cbde30a367032
palakpurohit7072-wq <tumhara-email@example.com>
2026-08-23 22:01:40 +0530

    Initial TypeScript project

---
+++ index.ts
@@ -0,0 +1,16 @@
+// let calfun = (a:number , b:number):number=>{
+// let sum = a + b;
+// return sum;
+// }
+// let result:number = calfun(3,"5");
+// console.log (result);
+
+// for checking typeOF
+let myname = 5;
+let namenumber = "palak";
+console.log(typeof myname.toString());
+
+//in case of NaN
+let name: number = NaN;
+let number = "palak";
+console.log( typeof name);
